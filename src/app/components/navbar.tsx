3a32da57a7ac9c423d5d475945caaf31f3033334
nam <help5432@naver.com>
2024-01-30 10:09:36 +0900

    feat:searchBar컴포넌트생성

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -8,11 +8,15 @@ type Props = {};
 
 export default function Navbar({}: Props) {
   return (
-    <div>
-      <div>Movies</div>
-      <div>
+    // flex 컨테이너 , max-witdh 1280px == 80rem == 7xl , w-full witdh 100% , 요소들에게 flex gap으로 요소들의 간격 설정
+    // justify-between div <-> div , items-center 요소 중앙배치 , py 패딩y축
+    <div className="flex max-w-7xl mx-auto w-full  justify-between items-center py-3 px-2">
+      {/* 4xl - fontsize , line-height */}
+      <div className="font-bold text-4xl">Movies</div>
+
+      <div className="flex gap-3">
         <Link href={"/"}>Home</Link>
-        {/* FavoritesPage와 Link 페이지연동 */}
+        {/* FavoritesPage와 Link 페이지연동 , Favorites 클릭시 http://localhost:3000/FavoritesPage이동 */}
         <Link href={"/FavoritesPage"}>Favorites</Link>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,11 @@ export default function Home() {
   return (
     <div>
       <Navbar />
+      {/* flex flex-col flex컨테이너 자식요소들을 세로배치 */}
+      <div className="max-w-7xl px-2 mx-auto flex flex-col">
+        <SearchBar />
+        <p>Hompage</p>
+      </div>
     </div>
   );
 }
