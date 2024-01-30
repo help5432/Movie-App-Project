ad5da518c9f870ce6c9f251cc3c19df3a58607ed
nam <help5432@naver.com>
2024-01-30 09:30:17 +0900

    feat:컴포넌트 navbar 생성 , favorites 폴더 및 page컴포넌트생성

---
+++ src/app/components/navbar.tsx
@@ -0,0 +1,19 @@
+//tsrfc
+import Link from "next/link";
+import React from "react";
+
+//Navbar 구성요소 , 왼쪽상단 Movies , 오른쪽상단 Home Favorites으로 간단하게 구성
+
+type Props = {};
+
+export default function Navbar({}: Props) {
+  return (
+    <div>
+      <div>Movies</div>
+      <div>
+        <Link href={"/"}>Home</Link>
+        <Link href={"/"}>Favorites</Link>
+      </div>
+    </div>
+  );
+}
